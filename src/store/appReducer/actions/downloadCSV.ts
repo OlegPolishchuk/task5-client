359775557eb72ae64ca478e4356dfa1f912ab7bc
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-11-28 20:30:19 +0300

    fix: .env base url

--- src/store/appReducer/actions/downloadCSV.ts
+++ src/store/appReducer/actions/downloadCSV.ts
@@ -6,13 +6,13 @@ export const downloadCSV = createAsyncThunk(
     try {
       const {data} = await appApi.downloadCSV();
 
-      const blob = new Blob([data], {});
+      const blob = new Blob([data], {type: 'text/plain',});
       const downloadUrl = window.URL.createObjectURL(blob);
 
       const link = document.createElement('a');
       link.href = downloadUrl;
 
-      link.setAttribute('download', 'data.cvs');
+      link.setAttribute('download', 'data.csv');
 
       document.body.appendChild(link);
 
